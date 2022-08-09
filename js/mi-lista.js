344c12ac73431cf5c65b8174ce7bbe2d1c37ef30
Manuel Barcena <manuel.barcena.sto@gmail.com>
2022-08-08 21:38:20 -0300

    Agregando Alertas

--- js/mi-lista.js
+++ js/mi-lista.js
@@ -1,30 +1,19 @@
-// Varibles del carrito
-const MI_LISTA = document.querySelector();
-const CONTENEDOR = document.querySelector();
-const VACIAR = document.querySelector();
-const LISTA = document.querySelector();
-let pelisLista = [];
-// Local Storage del carrito
-const GUARDAR_PRODUCTOS = [];
 
-registrarEventListeners();
-function registrarEventListeners(){
-    // Agregar peliculas usando "Agregar"
-    LISTA.addEventListener("click", agregarPelicula);
-    // Eliminar peliculas
-    MI_LISTA.addEventListener("click", eliminarPelicula);
-    // Vaciar lista
-    VACIAR.addEventListener("click", () => {
-        pelisLista = [];
-        limpiarHTML();
-    })
-};
+class Carrito{
+    agregarProductos(e){
+        e.preventDefault();
+        if(e.target.classList.contains("agregar-carrito")){
+            const producto = e.target.parentElement.parentElement;
+            this.leerDatosPelicula(producto)
+        }
+    }
 
-// Funciones
-function agregarPelicula(e) {
-    if(e.target.classList.contains("agregar mi lista")){
-        const PELICULAS_SELECCIONADA = e.target.parentElement;
-        leerProductos(PELICULAS_SELECCIONADA)
+    leerDatosProducto(producto){
+        const infoProducto = {
+            imagen : producto.querySelector("img").src,
+            titulo : producto.querySelector("h4"),textContent,
+        }
     }
 };
+
 
--- js/sesion.js
+++ js/sesion.js
@@ -47,5 +47,3 @@ function cerrarSesion(e){
 
 
 
-
-
